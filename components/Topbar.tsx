c1aa9a57274a8206212488ebd51d14a7fca6b05a
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-05-20 14:45:30 +0100

    Create the topbar component with its styles

---
+++ components/Topbar.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Topbar.module.css';
+
+export default function Topbar() {
+  return (
+    <div className={styles.main_container}>
+      <h1 className={styles.title}>Lavidalocal Enterprises</h1>
+    </div>
+  )
+}
